f4775b1b8af1512f761060a80e5d41c9ae512ca1
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2017-12-01 15:30:26 +0530

    updated readme fileand removed extra code

--- knn.py
+++ knn.py
@@ -28,15 +28,15 @@ dataset2= {'g1':['a','b','e','f'] ,'g2' :['c', 'b', 'e','f','a','g'], 'g3' :['x'
 
 
 
-
 vote_weight={'g1':[1,2,3,4], 'g2':[1,4,2,5,6,4],'g3':[1,4,2,5,6,3],'g4':[1,2,3,4,2,5,6,7,2], 
 	'g5':[1,2,4,0.5,6,4,1,3,5,5],'g6':[1,6,4,3,9,0,4,6,8,9,1.2,12,2,1],
 	'g7':[1,2,3,4], 'g8':[1,4,2,5,6,4],'g9':[1,4,2,5,6,3],'g10':[1,2,3,4], 'g11':[1,4,2,5,6,4],'g12':[1,4,2,5,6,3],
 	'g13':[1,2,3,4], 'g14':[1,4,2,5,6,4],'g15':[1,4,2,5,6,3], 'g16':[1,2,3,4], 'g17':[1,4,2,5,6,4],'g18':[1,4,2,5,6,3],
 	'g19':[1,2,3,4], 'g20':[1,4,2,5,6,4],'g21':[1,4,2,5,6,3], 'g22':[1,2,3,4], 'g23':[1,4,2,5,6,4],'g24':[1,4,2,5,6,3],
 	'g25':[1,2,3,4]}
+
 q_user='a'
-nearest_neightbour =10
+nearest_neighbour =10
 
 
 def k_nearest_neighbour(data,user,weight,n):
@@ -110,73 +110,5 @@ def k_nearest_neighbour(data,user,weight,n):
 	pl.show()
 
 
-	'''from scipy.interpolate import spline
-
-	T = np.array(req_plot_user)
-	power = np.array(req_plot_value)
-	plt.plot(T,power)
-	plt.show()
-
-
-	xnew = np.linspace(T.min(),T.max(),300) #300 represents number of points to make between T.min and T.max
-
-	power_smooth = spline(T,power,xnew)
-
-	plt.plot(xnew,power_smooth)
-	plt.show()'''
-
-
-
-
-
-result =k_nearest_neighbour(dataset2,q_user,vote_weight, nearest_neightbour)
-
-
-
-
-
-
-#mutiple line for loop different graphs
-#for i in dataset:
-	#for ii in dataset[i]:
-
-#single line for loop plotted in same graph	
-'''[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1])
-plt.show()
-'''
-
-'''def k_nearest_neighbors(data, predict, k=3):
-	if len(data)>=k:
-		warnings.warn("k is set to value less than total voting groups!")
-
-	distances=[]
-	for group in data:
-		for features in data[group]:
-
-			euclidean_distance=np.linalg.norm(np.array(features)-np.array(predict))
-			#euclidean_distance=np.sqrt(np.sum((np.array(features)-np.array(predict))**2))
-			#euclidean_distance =sqrt((features[0]-predict[0])**2 + (features[1]-predict[1])**2)
-
-			distances.append([euclidean_distance,group])
-
-
-	votes = [i[1] for i in sorted(distances)[:k]]
-
-	knn= (i[1] for i in sorted(distances)[:3])
-	print(votes[:k])
-	print(Counter(votes).most_common(1))
-
-	vote_result = Counter(votes).most_common(1)[0][0]
-
-	return vote_result
-
-result =k_nearest_neighbors(dataset, new_features, k=3)
-
-
-
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1], color=result)
-plt.show()'''
-
+result =k_nearest_neighbour(dataset2,q_user,vote_weight, nearest_neighbour)
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+Social voting based on K - nearest neighbours (Knn)
+
+Version used: python 3.6
+
+Package to be installed: 
+1. Numpy
+2. pandas
+3. pylab
+4. matplotlib
+
+Used U-G-U-V (User group user voting) algorithm to show which user are nearest based on the input:
+
+Input to be modified in code:
+
+q_user = " enter user for which you want to find nearest neighbour"
+
+nearest_neighbour= "number of nearest neighbour"
+
+
+Program description:
+
+There are 25 groups and each group have some members. Every member in group have some weight associated to them in that group(defined in vote_weight).
+
+Based on our query this program gives the nearest n neighbour to that user.
